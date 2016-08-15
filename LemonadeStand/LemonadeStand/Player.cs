4f5065dfbb2e4db3b51a83a97eb6f50c2d16624d
hilaryberigan <berigan.hilary@gmail.com>
2016-08-15 09:21:01 -0500

    first creation of class files, no substance yet

---
+++ LemonadeStand/LemonadeStand/Player.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Player
+    {
+        string name;
+        decimal totalDollars;
+        int score;
+
+        public Player(string name, decimal totalDollars)
+        {
+            this.name = name;
+            this.totalDollars = totalDollars;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+        public int GetScore()
+        {
+            return score;
+        }
+        
+        public void AddOneToScore()
+        {
+            score++;
+        }
+        public decimal GetTotalDollars()
+        {
+            return totalDollars;
+        }
+
+        public void ChangeTotalDollars()
+        {
+            totalDollars + //pull in price of supplies supplyName.GetPrice
+        }
+    }
+}
---
+++ LemonadeStand/LemonadeStand/Supply.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Supply
+    {
+        decimal price;
+        int totalNumber;
+        string type;
+
+        public Supply(string type, decimal price)
+        {
+            this.type = type;
+            this.price = price;
+            totalNumber = 0;
+        }
+
+        public string GetType()
+        {
+            return this.type;
+        }
+        public 
+
+    }
+}
